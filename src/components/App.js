472436189b35d88e11db4e020e7534c4617cd1ce
HY Lim <hylim@HYs-MacBook-Air.local>
2022-01-21 01:09:32 +0800

    Modify code to complete the DApp Add new video stock for demo purpose

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     await this.loadBlockchainData()
   }
 
+  /* web3 standard API that take metamask Eth account and inject to the code*/
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
@@ -33,60 +34,134 @@ class App extends Component {
     const web3 = window.web3
     //Load accounts
     //Add first account the the state
-
-    //Get network ID
-    //Get network data
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    //Store first item of accounts 
+    this.setState({account: accounts[0]})
+    //Get network ID (Configured in metamask)
+    const networkId = await web3.eth.net.getId()
+    //Get network data from generated JSON of smart contract
+    const networkData = DVideo.networks[networkId] 
     //Check if net data exists, then
       //Assign dvideo contract to a variable
-      //Add dvideo to the state
+      if(networkData){
+        const dvideo = new web3.eth.Contract(DVideo.abi,networkData.address)
+        //Add dvideo to the state
+        this.setState({dvideo}) // if same name of state object and local var like above, can write as such
+        //Check videoAmounts
+        const videosCount = await dvideo.methods.videoCount().call() // Call this function/property that is publicly available
+        //Add videAmounts to the state
+        this.setState({videosCount})
+        //Iterate throught videos and add them to the state (by newest-last index)
+        for(var i=videosCount;i>=1;i--){
+          const video = await dvideo.methods.videos(i).call() // Call this properties based on index. This is sort of like simple database object with index
+          this.setState({
+            // Add newly load video to the state array to the last index
+            videos:[...this.state.videos, video]
+          })
+        }
 
-      //Check videoAmounts
-      //Add videAmounts to the state
+        //Set latest video and it's title to view as default 
+        const latest = await dvideo.methods.videos(videosCount).call()
+        this.setState({
+          currentHash: latest.hash,
+          currentTitle: latest.title
+        })
+        //Set loading state to false
+        this.setState({loading:false})
 
-      //Iterate throught videos and add them to the state (by newest)
-
-
-      //Set latest video and it's title to view as default 
-      //Set loading state to false
-
-      //If network data doesn't exisits, log error
+      }else{  
+        window.alert('DVideo smart contract is not deployed to detected network.')
+      }    
+      
   }
 
   //Get video
   captureFile = event => {
-
+    event.preventDefault()
+    // Get file from input and convert to array buffer
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    
+    // function when reader finished loaded
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result)
+      })
+      console.log('buffer', this.state.buffer)
+    }
   }
 
   //Upload video
   uploadVideo = title => {
-
+    console.log('Submitting file to IPFS')
+    // Add to IPFS
+    this.setState({loading: true})
+    ipfs.add(this.state.buffer,(error,result)=>{
+      //callback function
+      //Put on blockchain on hash address in IPFS
+      if(error){
+        // Error handling
+        console.log('Error while uploading to IPFS', error)
+        this.setState({loading: true})
+        return
+      } 
+      
+      else {
+         console.log('IPFS result', result)
+         // Get hash address from result first array element and title from event
+         // to update state in smart contract, we have to use send and set sender address
+         this.state.dvideo.methods.uploadVideo(result[0].hash,title).send({from: this.state.account})
+         .on('transactionHash',(hash)=>{
+           // callback function after send function on transactionHash event
+           this.setState({loading: false})
+           window. location. reload() 
+         })
+      }      
+    })
   }
 
   //Change Video
   changeVideo = (hash, title) => {
-
+    this.setState({'currentHash': hash})
+    this.setState({'currentTitle': title})
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      loading: false
+      loading: true,
+      account: '',
+      dvideo:null,
+      videos:[],
+      buffer:null,
+      currentHash:null,
+      currentTitle:null
       //set states
     }
 
     //Bind functions
+    this.uploadVideo = this.uploadVideo.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+    this.changeVideo = this.changeVideo.bind(this)
   }
 
   render() {
     return (
       <div>
-        <Navbar 
+        <Navbar account={this.state.account}
           //Account
         />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-              //states&functions
+          : <Main 
+              captureFile={this.captureFile} // Link property to event
+              uploadVideo={this.uploadVideo }
+              changeVideo={this.changeVideo}
+              currentHash={this.state.currentHash}
+              currentTitle={this.state.currentTitle}
+              videos={this.state.videos}
             />
         }
       </div>
--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,53 @@
 pragma solidity ^0.5.0;
 
+// Workflow of smart contract
+// 1. Model the video struct
+// 2. Store the video 
+// 3. Upload the video
+// 4. List video
+
 contract DVideo {
   uint public videoCount = 0;
   string public name = "DVideo";
-  //Create id=>struct mapping
+  
+  //Create id=>struct mapping (store video)
+  // mapping is default solidity to assign unit to each Video class and return as videos
+  mapping(uint=>Video) public videos;
 
-  //Create Struct
-
+  //Create Struct video (model video struct)
+  struct Video {
+    uint id;          // unsigned int of identifier of video
+    string hash;      // ipfs hash address
+    string title;     // video title    
+    address author;   // smart contract address of author
+  }
 
   //Create Event
-
+  event VideoUploaded (
+    uint id,          
+    string hash,   
+    string title,     
+    address author   
+  );
 
   constructor() public {
   }
 
   function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
-
+    // Make sure the video hash exists - require() equivalent to if else statement 
+    require(bytes(_videoHash).length >0);
     // Make sure video title exists
-
+    require(bytes(_title).length >0);
     // Make sure uploader address exists
-
+    require(msg.sender!=address(0));
 
     // Increment video id
-
-    // Add video to the contract
-
+    videoCount++;
+    // Add video to the contract (Constructor of video struct)
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+    
     // Trigger an event
-
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
   }
+
 }
