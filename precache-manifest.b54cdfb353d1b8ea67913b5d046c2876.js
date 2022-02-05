6285dac630b868889dff6e20c26cd880d762332e
HY Lim <hylim@HYs-MacBook-Air.local>
2022-02-05 18:09:48 +0800

    Updates

---
+++ precache-manifest.b54cdfb353d1b8ea67913b5d046c2876.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b488bed8b4911c3ad9648e832e3f0c74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83c68053184e8e44d3a2",
+    "url": "./static/css/2.7b4682b0.chunk.css"
+  },
+  {
+    "revision": "e2c61240a5108bddbdd4",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "83c68053184e8e44d3a2",
+    "url": "./static/js/2.344a2e7d.chunk.js"
+  },
+  {
+    "revision": "ff7fc069db78fe2b8d67d3678cff1820",
+    "url": "./static/js/2.344a2e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c61240a5108bddbdd4",
+    "url": "./static/js/main.922fa562.chunk.js"
+  },
+  {
+    "revision": "dec62e2ff5de633287f9",
+    "url": "./static/js/runtime-main.3652fba8.js"
+  },
+  {
+    "revision": "9e02a88e54fb14899a2ea3c60bf8fa35",
+    "url": "./static/media/dvideo.9e02a88e.png"
+  }
+]);
